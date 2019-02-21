8b460dbe47300e7b3a7558106534bba8f4d78880
pshu <24f1989@gmail.com>
2019-02-22 02:21:59 +0300

    restore mime version

--- test/test.js
+++ test/test.js
@@ -1,32 +1,28 @@
-var assert   = require('assert');
-var gutil    = require('gulp-util');
-var cssfont64 = require('../index');
-var fs       = require('fs');
-var path     = require('path');
+var assert = require("assert");
+var cssfont64 = require("../index");
+var fs = require("fs");
+var path = require("path");
+var Vinyl = require("vinyl");
 
-describe('gulp-cssfont64', function() {
-	describe('in buffer mode', function() {
+describe("gulp-cssfont64", function() {
+    describe("in buffer mode", function() {
+        it("should encode fonts to base64 and generate a css file", function(done) {
+            var filename = path.join(__dirname, "/fixtures/my+font.ttf");
 
-		it('should encode fonts to base64 and generate a css file', function(done) {
+            var input = new Vinyl({
+                base: path.dirname(filename),
+                path: filename,
+                contents: new Buffer(fs.readFileSync(filename, "utf8"))
+            });
 
-			var filename = path.join(__dirname, '/fixtures/my+font.ttf');
+            var stream = cssfont64();
 
-			var input = new gutil.File({
-				base: path.dirname(filename),
-				path: filename,
-				contents: new Buffer(fs.readFileSync(filename, 'utf8'))
-			});
+            stream.on("data", function(newFile) {
+                assert.equal(String(newFile.contents), fs.readFileSync(path.join(__dirname, "/fixtures/my+font.css"), "utf8"));
+                done();
+            });
 
-			var stream = cssfont64();
-
-			stream.on('data', function(newFile) {
-				assert.equal(String(newFile.contents), fs.readFileSync(path.join(__dirname, '/fixtures/my+font.css'), 'utf8'));
-				done();
-			})
-
-			stream.write(input);
-
-		});
-
-	});
+            stream.write(input);
+        });
+    });
 });
